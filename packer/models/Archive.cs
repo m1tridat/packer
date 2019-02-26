a52e7c16d47c191dad11c3d3c7b30a3b9733e9cd
m1tridat <akzhol@mail.com>
2019-02-26 10:38:31 +0600

    Archive.cs

---
+++ packer/models/Archive.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace packer
+{
+    class Archive
+    {
+
+        public string defaultName = "pucker.zip";
+        public string Name;
+
+        public List<string> files;
+        public List<string> folders;
+
+        public string defaulOutputPath = Environment.SpecialFolder.Desktop.ToString();
+
+
+        public Archive()
+        {
+            this.Name = defaultName;
+        }
+
+        public Archive (string name)
+        {
+            this.Name = name;
+        }
+
+
+        public void addFileToArchive(string file)
+        {
+            this.files.Add(file);
+        }
+
+        public void addFolderToArchive(string folder)
+        {
+            this.folders.Add(folder);
+        }
+
+
+
+    }
+}
